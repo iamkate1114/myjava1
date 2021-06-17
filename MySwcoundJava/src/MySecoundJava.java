7b59425307810b7b8f40a9b993f88a5a1a69462c
TJ <TJ@DESKTOP-0S1SB1F>
2021-06-17 22:02:32 +0900

    변수

--- MySwcoundJava/src/MySecoundJava.java
+++ MySwcoundJava/src/MySecoundJava.java
@@ -9,14 +9,24 @@ public class MySecoundJava {
 	 * @param args  <- 아랫줄에 대한 args라는 매소드에 대해 설명좀 해줘~ -> 해당 코드 줄 위에서 파란주석
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World2");
-		System.out.println("한냥글");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
+		int mynumber;
+		mynumber = 10;
+		
+		int mynumber2 = 10;
+		
+		System.out.println("mynumber의 값은"+mynumber);
+		System.out.println("mynumber2의 값은"+mynumber2);
+		
+		String name = "홍길동";
+		String name2 = null;
+		String name3 = "";
+		
+		System.out.println("name의 값은"+name);
+		System.out.println("name2의 값은"+name2);
+		System.out.println("name3의 값은"+name3);
+		
+		
 	}
+	
 
 }
